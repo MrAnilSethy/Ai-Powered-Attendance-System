6ab2d39feee9a595588b03a271db0d809fdc3c52
Anil Sethy <anilsethy470@gmail.com>
2026-08-01 11:52:11 +0530

    modified domain name

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from src.screens.teacher_screen import teacher_screen
 from src.screens.student_screen import student_screen
 from src.components.dialog_auto_enroll import auto_enroll_dialog
 def main():
+    
+    st.set_page_config(
+        page_title='SnapClass - Making Attendance faster using AI',
+        page_icon='https://i.ibb.co/YTYGn5qV/logo.png'
+    )
     if 'login_type' not in st.session_state:
         st.session_state['login_type'] = None
     match st.session_state['login_type']:
